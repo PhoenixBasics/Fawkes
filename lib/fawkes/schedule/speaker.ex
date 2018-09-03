ff94b32856b25dbebe2a1e4582b28102691df5e8
Nhu Nguyen <nhu313@gmail.com>
2018-09-03 06:46:40 -0400

    updated speaker

--- lib/fawkes/schedule/speaker.ex
+++ lib/fawkes/schedule/speaker.ex
@@ -1,14 +1,14 @@
 defmodule Fawkes.Schedule.Speaker do
   use Ecto.Schema
   import Ecto.Changeset
-
+  use Arc.Ecto.Schema
 
   schema "profiles" do
     field :company, :string
     field :description, :string
     field :first, :string
     field :github, :string
-    field :image, :string
+    field(:image, Fawkes.Uploader.Image.Type)
     field :image_url, :string
     field :last, :string
     field :slug, :string
@@ -22,6 +22,7 @@ defmodule Fawkes.Schedule.Speaker do
   def changeset(speaker, attrs) do
     speaker
     |> cast(attrs, [:slug, :image, :image_url, :first, :last, :company, :github, :twitter, :description, :talk_id])
+    |> cast_attachments(attrs, [:image])
     |> validate_required([:slug, :image_url, :first, :last])
     |> unique_constraint(:slug)
   end
